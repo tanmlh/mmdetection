c5761051fd30c51862f5ccd980e99d4444efce6c
tanmlh <fahongzhang@gmail.com>
2024-09-04 21:55:48 +0000

    a

--- configs/crowd_ai/mask2former_r50_query-300_100e_crowd_ai.py
+++ configs/crowd_ai/mask2former_r50_query-300_100e_crowd_ai.py
@@ -228,4 +228,4 @@ visualizer = dict(
 #   - `enable` means enable scaling LR automatically
 #       or not by default.
 #   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
-auto_scale_lr = dict(enable=True, base_batch_size=16)
+auto_scale_lr = dict(enable=False, base_batch_size=16)
