919c4df62eb8c9e19ff7f55b10c65b41373c1bc0
tanmlh <fahongzhang@gmail.com>
2024-09-28 19:46:05 +0000

    a

--- configs/crowd_ai/polygonizer_v20_gcp-gt_cv2_no-dice_angle-loss_lam-4_r50_query-100_80k_crowd_ai.py
+++ configs/crowd_ai/polygonizer_v20_gcp-gt_cv2_no-dice_angle-loss_lam-4_r50_query-100_80k_crowd_ai.py
@@ -298,7 +298,19 @@ optim_wrapper = dict(
         norm_decay_mult=0.0),
     clip_grad=dict(max_norm=0.01, norm_type=2))
 
-max_epochs=6
+
+max_iters = 80000
+interval = 5000
+dynamic_intervals = [(max_iters // interval * interval + 1, max_iters)]
+train_cfg = dict(
+    type='IterBasedTrainLoop',
+    max_iters=max_iters,
+    val_interval=interval,
+    dynamic_intervals=dynamic_intervals)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+# learning rate
 param_scheduler = [
     # dict(
     #     type='LinearLR', start_factor=0.001, by_epoch=False, begin=0,
@@ -306,15 +318,31 @@ param_scheduler = [
     dict(
         type='MultiStepLR',
         begin=0,
-        end=max_epochs,
-        by_epoch=True,
-        milestones=[4],
+        end=max_iters,
+        by_epoch=False,
+        milestones=[40000, 75000],
         gamma=0.1)
 ]
 
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1)
-val_cfg = dict(type='ValLoop')
-test_cfg = dict(type='TestLoop')
+# max_epochs=6
+# param_scheduler = [
+#     # dict(
+#     #     type='LinearLR', start_factor=0.001, by_epoch=False, begin=0,
+#     #     end=1000),
+#     dict(
+#         type='MultiStepLR',
+#         begin=0,
+#         end=max_epochs,
+#         by_epoch=True,
+#         milestones=[4],
+#         gamma=0.1)
+# ]
+
+# train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1)
+# val_cfg = dict(type='ValLoop')
+# test_cfg = dict(type='TestLoop')
+
+
 log_processor = dict(type='LogProcessor', window_size=50, by_epoch=True)
 
 default_hooks = dict(
@@ -334,7 +362,7 @@ vis_backends = [
         init_kwargs=dict(
             project = 'mmdetection',
             entity = 'tum-tanmlh',
-            name = 'gcp_gt_polygonizer_v20_gpu-2_cv2_no-dice_angle-loss_lam-4_r50_query-100_6e_crowd_ai',
+            name = 'gcp_gt_polygonizer_v20_gpu-2_cv2_no-dice_angle-loss_lam-4_r50_query-100_80k_crowd_ai',
             resume = 'never',
             dir = './work_dirs/',
             allow_val_change=True
